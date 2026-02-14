e380da6a3480f27935e9887f435ee26f2712ce2a
Jamie2166687 <up2166687@myport.ac.uk>
2026-02-14 17:13:15 +0000

    Player & Cargo WIP 2
    +4 empty child classes of AbstractObstacle
    +Method & int with getter & setter in AbstractObstacle that functions as health
    
    +Methods in PlayerStats for receiving cargo & damaging cargo
    +Incomplete method for delivering cargo
    +Empty OnTriggerEnter to handle calling CargoDamage()
    
    TODO: Obstacle triggers
    +-Unsure if OnTriggerEnter to handle calling CargoDamage() should be implemented in PlayerStats or the obstacle classes
    +-Conditionals need to be implemented to check if the collided obstacle should call CargoDamage(), depending on the value of HeldCargo

--- Assets/Scripts/Cargo/AbstractCargo.cs
+++ Assets/Scripts/Cargo/AbstractCargo.cs
@@ -2,13 +2,23 @@ using UnityEngine;
 
 public abstract class AbstractCargo : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    private int cargoCount;
 
-    void Update()
+    public void SetCargoCount(int amount)
     {
-        
+        cargoCount = amount;
+    }
+    public int GetCargoAmount()
+    {
+        return cargoCount;
+    }
+    public void TakeDamage() ///will probably implement interface to handle damage method as player will also need it
+    {
+        cargoCount--;
+
+        if (cargoCount < 0)
+        {
+            cargoCount = 0;
+        }
     }
 }
--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -2,12 +2,68 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    public AbstractCargo[] HeldCargo;
+    private AbstractCargo HeldCargo;
+
+    public void CargoNew(AbstractCargo cargo, int amount)   //Called when recieving cargo from island
+    {
+        if (HeldCargo != null)  //Cargo will be ignored if the player is already carrying cargo
+        {
+            Debug.Log($"Cargo declined, player is carrying {HeldCargo}, {HeldCargo.GetCargoAmount()}");
+        }
+        else
+        {
+            HeldCargo = cargo;
+            HeldCargo.SetCargoCount(amount);
+            Debug.Log($"Player now has {HeldCargo}, {HeldCargo.GetCargoAmount()}");
+        }
+    }
+
+    public void CargoDeliver()
+    {
+        if (HeldCargo != null)
+        {
+            ///add to some kind of score manager depending on amount of cargo delivered??
+            
+            Destroy(HeldCargo);
+            HeldCargo = null;
+        }
+        else
+        {
+            Debug.Log("Could not deliver cargo, player has none");
+        }
+    }
+
+    public void CargoDamage()   //Called by OnTriggerEnter when colliding with valid obstacle
+    {
+        HeldCargo.TakeDamage();
+
+        if (HeldCargo.GetCargoAmount() == 0)
+        {
+            Debug.Log("Cargo has been destroyed");
+            Destroy(HeldCargo);
+            HeldCargo = null;
+        }
+    }
+
+
+    ///not sure if OnTriggerEnter to damage cargo should be called here or in the obstacle?
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle") == true)
+        {
+            ///conditionals to check if correct kind of obstacle to damage cargo
+            //CargoDamage();
+        }
+    }
+
+    private void Awake()
+    {
+        
+    }
     void Start()
     {
         
     }
-
     void Update()
     {
         
