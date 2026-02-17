f0e295623f125c9d4053f39183466318f1d281d2
Jamie2166687 <up2166687@myport.ac.uk>
2026-02-17 23:05:40 +0000

    Player & Cargo WIP 3
    +-Reimplemented getter & setter for cargoCount in AbstractCargo
    
    +Added switch case in OnTriggerEnter to check for obstacle name, and then run CargoDamage() depending on the value of _heldCargo
    +-Changed HeldCargo variable in PlayerStats to _heldCargo to better fit the editor config
    
    Very small push to fix implementations in previous push, need to discuss what needs to be worked on next
    
    Possibly add bools or some kind of value in AbstractCargo subclasses for PlayerStats to check to see if the cargo can be damaged by a certain type of obstacle, instead of checking the kind of cargo in the switch case. Otherwise, the subclasses will remain empty, and it seems pointless to use subclasses instead of assigning a string to PlayerStats' cargo.

--- Assets/Scripts/Cargo/AbstractCargo.cs
+++ Assets/Scripts/Cargo/AbstractCargo.cs
@@ -3,22 +3,20 @@ using UnityEngine;
 public abstract class AbstractCargo : MonoBehaviour
 {
     private int cargoCount;
-
-    public void SetCargoCount(int amount)
+    public int CargoCount
     {
-        cargoCount = amount;
-    }
-    public int GetCargoAmount()
-    {
-        return cargoCount;
+        get { return cargoCount; }
+        set
+        {
+            cargoCount = value;
+            if (cargoCount < 0)
+            {
+                cargoCount = 0;
+            }
+        }
     }
     public void TakeDamage() ///will probably implement interface to handle damage method as player will also need it
     {
-        cargoCount--;
-
-        if (cargoCount < 0)
-        {
-            cargoCount = 0;
-        }
+        CargoCount--;
     }
 }
--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,31 +1,32 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    private AbstractCargo HeldCargo;
+    private AbstractCargo _heldCargo;
 
     public void CargoNew(AbstractCargo cargo, int amount)   //Called when recieving cargo from island
     {
-        if (HeldCargo != null)  //Cargo will be ignored if the player is already carrying cargo
+        if (_heldCargo != null)  //Cargo will be ignored if the player is already carrying cargo
         {
-            Debug.Log($"Cargo declined, player is carrying {HeldCargo}, {HeldCargo.GetCargoAmount()}");
+            Debug.Log($"Cargo declined, player is carrying {_heldCargo}, {_heldCargo.CargoCount}");
         }
         else
         {
-            HeldCargo = cargo;
-            HeldCargo.SetCargoCount(amount);
-            Debug.Log($"Player now has {HeldCargo}, {HeldCargo.GetCargoAmount()}");
+            _heldCargo = cargo;
+            _heldCargo.CargoCount = amount;
+            Debug.Log($"Player now has {_heldCargo}, {_heldCargo.CargoCount}");
         }
     }
 
     public void CargoDeliver()
     {
-        if (HeldCargo != null)
+        if (_heldCargo != null)
         {
             ///add to some kind of score manager depending on amount of cargo delivered??
             
-            Destroy(HeldCargo);
-            HeldCargo = null;
+            Destroy(_heldCargo);
+            _heldCargo = null;
         }
         else
         {
@@ -35,24 +36,54 @@ public class PlayerStats : MonoBehaviour
 
     public void CargoDamage()   //Called by OnTriggerEnter when colliding with valid obstacle
     {
-        HeldCargo.TakeDamage();
+        _heldCargo.TakeDamage();
 
-        if (HeldCargo.GetCargoAmount() == 0)
+        if (_heldCargo.CargoCount == 0)
         {
             Debug.Log("Cargo has been destroyed");
-            Destroy(HeldCargo);
-            HeldCargo = null;
+            Destroy(_heldCargo);
+            _heldCargo = null;
         }
     }
 
-
-    ///not sure if OnTriggerEnter to damage cargo should be called here or in the obstacle?
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Obstacle") == true)
         {
-            ///conditionals to check if correct kind of obstacle to damage cargo
-            //CargoDamage();
+            switch (other.name) ///adjust conditionals to match obstacles when they are implemented
+            {
+                case "Rock":
+                    CargoDamage();
+                    break;
+
+                case "Rain":
+                    if (_heldCargo is FruitCargo || _heldCargo is BookCargo)
+                    {
+                        CargoDamage();
+                    }
+                    break;
+
+                case "Wave":
+                    if (_heldCargo is FruitCargo || _heldCargo is AnimalCargo || _heldCargo is GlassCargo)
+                    {
+                        CargoDamage();
+                    }
+                    break;
+
+                case "Cold":
+                    if (_heldCargo is AnimalCargo)
+                    {
+                        CargoDamage();
+                    }
+                    break;
+
+                case "Hot":
+                    if (_heldCargo is GlassCargo)
+                    {
+                        CargoDamage();
+                    }
+                    break;
+            }
         }
     }
 
