41a65482d3cbe3fe6e4b48002d82797437b3eae6
PD-2272835 <186196474+PD-2272835@users.noreply.github.com>
2026-04-20 13:27:57 +0100

    Basic Obstacle Picking based on weight
    +Weighted Obstacle Picking
    +Spawn Obstacle Stub (need full implementation
    +Island Spawning Check Method (also needs implementation)
    
    need to change EnterState to fully initialize gameplay correctly

--- Assets/Scripts/GameStateManager/GameplayState.cs
+++ Assets/Scripts/GameStateManager/GameplayState.cs
@@ -4,19 +4,29 @@ public class GameplayState : AbstractGameState
 {
 
     public int CurrentScore = 0;
-    private float ObstacleRandomXOffet => Random.Range(1, 20);
+    private float ObstacleRandomXOffet => Random.Range(1, 20); //like lanes, will be relative to player
 
-    private float _DistanceCovered = 0;
-    private readonly float _YBetweenObstacles = 30;
+    private float _DistanceCovered = 0f;
+    private float _LastIslandLocation = 0f;
 
-    public float InitialPlayerForwardSpeed = 10;
+    private float _ElapsedTime = 0f;
+    private float _TotalObstacleSpawnWeight = 0f;
 
+    private float _InitialSpawnInterval = 100f;
+    private float _CurrentSpawnInterval; //time between obstacles
+    private float _DifficultyRampDuration = 300f; //seconds to get to max difficulty
+    private float _MinSpawnInterval = 0.5f; //minimum time between obstacles (seconds)
 
+    //this should reset/initialze the game state
     public override void EnterState(GameStateManager context)
     {
-        GameStateManager.Instance.PlayerForwardSpeed = InitialPlayerForwardSpeed; //reset players speed on gameplay start
+        context.PlayerForwardSpeed = 10f; //reset players speed on gameplay start
+        _CurrentSpawnInterval = 0f;
 
-
+        foreach(var data in context.allObstacles)
+        {
+            _TotalObstacleSpawnWeight += data.SpawnWeight;
+        }
     }
 
     public override void ExitState(GameStateManager context)    //GameStateManager should retrieve CurrentScore before exiting ExitState
@@ -26,14 +36,65 @@ public class GameplayState : AbstractGameState
 
     public override void Update(GameStateManager context)
     {
+        _ElapsedTime += Time.deltaTime;
+        float t = Mathf.Clamp01(_ElapsedTime / _DifficultyRampDuration);
+        _CurrentSpawnInterval = Mathf.Lerp(_InitialSpawnInterval, _MinSpawnInterval, t);
+
+
+        //flow for this is as follows
         //pick an obstacle to spawn
-        //-check elapsed distance between island checkpoints and set flag
+        //-check elapsed distance:speed between island checkpoints and set flag
         //-allow picking island only if flag active
         //-otherwise only use weather/misc obstacles
         //get pool for that obstacle
+        //find an appropriate location to spawn the selected obstacle
         //spawn it
+        ObstacleSettings obstacle = PickWeightedObstacle();
+
+        if (obstacle.Kind != ObstacleKind.Island)
+        {
+            SpawnObstacle(obstacle);
+        }
+        else
+        {
+            if (CanSpawnIsland())
+            {
+                SpawnObstacle(obstacle);
+            }
+        }
     }
 
 
 
+    private ObstacleSettings PickWeightedObstacle()
+    {
+        float roll = Random.Range(0, _TotalObstacleSpawnWeight);
+        float cumulative = 0;
+        foreach(var data in GameStateManager.Instance.allObstacles)
+        {
+            if (roll <= cumulative)
+            {
+                return data;
+            }
+            cumulative += data.SpawnWeight;
+        }
+
+        //safe exit if no valid obstacle is found
+        //this means that allObstacles should probably start with a safe object, like a rock
+        return GameStateManager.Instance.allObstacles[0];
+    }
+
+
+    private void SpawnObstacle(ObstacleSettings settings)
+    {
+        Flyweight obstacle = FlyweightFactory.Spawn(settings);
+        obstacle.transform.position = Vector3.zero; //set new obstacle position
+        //TODO: determine position
+    }
+
+    private bool CanSpawnIsland()
+    {
+        //TODO: island placement test - based on distance from last island?
+        return false;
+    }
 }
