6e184d3f8aee622bc02ea82bca913dbdb7f99dbc
PD-2272835 <186196474+PD-2272835@users.noreply.github.com>
2026-06-06 17:36:47 +0100

    Cloud Fix + Tutorial Tooltip thingy
    ~Added TRS node to cloud prefabs so they appear and actually end up in the sky
    +Tooltip enabling/disabling based on progress through tutorial via index (this can be configured in the Camera Controller attached to the main camera)

--- Assets/Scripts/Player/TutorialCameraController.cs
+++ Assets/Scripts/Player/TutorialCameraController.cs
@@ -1,14 +1,19 @@
 using System.Collections.Generic;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class TutorialCameraController : MonoBehaviour
 {
+
+    //IMPORTANT: THESE SHOULD LINE UP AND CORRESPOND WITH EACH OTHER
     [SerializeField] private List<Transform> _Positions;
+    [SerializeField] private List<GameObject> _Tooltips;
+
+    //camera movement parameters
     [SerializeField] private float _MovementSpeed = 100f;
     [SerializeField] private float _RotationSpeed = 10f;
 
+    //track the current state of the tutorial
     [SerializeField] private bool _atTargetTransform = false;
     [SerializeField] private int _targetIndex;
 
@@ -39,11 +44,13 @@ public class TutorialCameraController : MonoBehaviour
             transform.SetPositionAndRotation(Vector3.MoveTowards(transform.position, _Positions[_targetIndex].position, _MovementSpeed * Time.deltaTime),
                 Quaternion.RotateTowards(transform.rotation, _Positions[_targetIndex].rotation, _RotationSpeed * Time.deltaTime));
 
-            //check to see if we are at our target position/orientation
+            //is the cameara at the target position/orientation?
             if (Vector3.SqrMagnitude(_Positions[_targetIndex].position - transform.position) < 1.0f //SqrDistance is cheaper to calc
-                && transform.rotation == _Positions[_targetIndex].rotation) //could be better? might need to be less precise to prevent errors
+                && transform.rotation == _Positions[_targetIndex].rotation) //could be better? might need to be less precise to prevent jitter
             {
                 _atTargetTransform = true; //if we are, stop moving the camera, if not, continue
+                SetAllBillboardsEnabled(false); //disable all billboards
+                _Tooltips[_targetIndex].SetActive(true); //enable the billboard for current tooltip
             }
         }
     }
@@ -53,5 +60,13 @@ public class TutorialCameraController : MonoBehaviour
         if(_targetIndex == index) return;
         _targetIndex = index;
         _atTargetTransform = false;
+    }
+
+    private void SetAllBillboardsEnabled(bool enabled)
+    {
+        foreach (GameObject board in _Tooltips)
+        {
+            board.SetActive(enabled);
+        }
     }
 }
